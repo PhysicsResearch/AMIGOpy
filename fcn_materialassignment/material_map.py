c636fd81e32ffa9a574fda09bf7d6b60d8b53772
MAASTRO\tommaso.siligardi <tsiligardi@student.maastrichtuniversity.nl>
2025-07-01 12:08:07 +0200

    minor changes

---
+++ fcn_materialassignment/material_map.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  1 12:01:02 2025
+
+@author: tommaso.siligardi
+"""
+
+import pandas as pd
+import numpy as np
+from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox
+import csv, math
+from PyQt5.QtGui         import QColor, QBrush
+from PyQt5.QtCore        import Qt
+
+def update_material_list(self):
+    pass
+
+
+def mat2HU(self):
+    pass
+
+def generate_mat_map(self):
+    pass
