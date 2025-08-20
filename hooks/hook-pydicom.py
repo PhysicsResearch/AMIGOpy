6d54c33acca99c9a585177c4f91165f3269b1067
UNIMAAS\gabriel.paivafonseca <g.paivafonseca@maastrichtuniversity.nl>
2025-08-20 12:25:54 +0200

    compile again

--- hooks/hook-pydicom.py
+++ hooks/hook-pydicom.py
@@ -1,4 +1,11 @@
-from PyInstaller.utils.hooks import collect_submodules
+# hooks/hook-pydicom.py
+from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
-# Collect all submodules of pydicom
-hiddenimports = collect_submodules('pydicom')
+# Bring in pixel pipeline pieces that are sometimes imported dynamically
+hiddenimports = []
+hiddenimports += collect_submodules("pydicom.pixels")
+hiddenimports += collect_submodules("pydicom.pixels.decoders")
+hiddenimports += collect_submodules("pydicom.encaps")
+
+# Include pydicom's data directory (urls.json etc.)
+datas = collect_data_files("pydicom", includes=["data/*"])
