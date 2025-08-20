83b9dfdfc528aec14aae488454f1112638feb55f
UNIMAAS\gabriel.paivafonseca <g.paivafonseca@maastrichtuniversity.nl>
2025-08-20 17:48:09 +0200

    no message

--- hooks/hook-pydicom.py
+++ hooks/hook-pydicom.py
@@ -1,11 +1,27 @@
-# hooks/hook-pydicom.py
-from PyInstaller.utils.hooks import collect_submodules, collect_data_files
+from PyInstaller.utils.hooks import (
+    collect_submodules,
+    collect_data_files,
+    collect_dynamic_libs,
+)
 
-# Bring in pixel pipeline pieces that are sometimes imported dynamically
+# --- pydicom: dynamic imports and data ---
 hiddenimports = []
+# pixel pipeline (dynamic)
 hiddenimports += collect_submodules("pydicom.pixels")
 hiddenimports += collect_submodules("pydicom.pixels.decoders")
 hiddenimports += collect_submodules("pydicom.encaps")
 
-# Include pydicom's data directory (urls.json etc.)
+# encoders (NEW) -> this is where pydicom.encoders.gdcm lives
+hiddenimports += collect_submodules("pydicom.encoders")
+# be explicit too (helps some PyInstaller versions)
+hiddenimports += ["pydicom.encoders.gdcm"]
+
+# package data (urls.json etc.)
 datas = collect_data_files("pydicom", includes=["data/*"])
+
+# --- python-gdcm package (module name 'gdcm') ---
+# bring the package, plus its DLLs
+hiddenimports += collect_submodules("gdcm")
+binaries = collect_dynamic_libs("gdcm")           # .dll/.pyd
+# (package has no extra data files normally, but safe to include)
+datas += collect_data_files("gdcm", include_py_files=False)
